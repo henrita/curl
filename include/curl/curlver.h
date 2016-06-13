b7e4c5889394fb61e2546f05becb308094696302
Henri Hein <henri@thinair.com>
2016-06-13 12:36:52 -0700

    Stamp tafork in version info.

--- include/curl/curlver.h
+++ include/curl/curlver.h
@@ -30,7 +30,7 @@
 
 /* This is the version number of the libcurl package from which this header
    file origins: */
-#define LIBCURL_VERSION "7.50.0-DEV"
+#define LIBCURL_VERSION "7.50.0-DEV-tafork"
 
 /* The numeric version number is also available "in parts" by using these
    defines: */
